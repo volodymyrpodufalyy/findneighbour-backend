a061f2fed422eaabf84b2cdc37e8743127c9ea60
Volodymyr Podufalyy <vpodufalyy13@gmail.com>
2021-12-07 14:18:48 +0200

    cors update

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ const options = {
 const swaggerSpec = swaggerJSDoc(options);
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use(cors());
+app.use(cors({ origin: true, credentials: true }));
 
 const start = async () => {
   try {
