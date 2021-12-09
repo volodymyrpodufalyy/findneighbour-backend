5eea30f4a559ddf115210b6e42ea3ca5ae9b3ddc
Volodymyr Podufalyy <vpodufalyy13@gmail.com>
2021-12-09 21:12:12 +0200

    feature/upload controller merge changes

--- src/controllers/UploadController.ts
+++ src/controllers/UploadController.ts
@@ -4,10 +4,8 @@ import { UploadModel } from "../models";
 import cloudinary from "../core/cloudinary";
 
 class UploadController {
-<<<<<<< Updated upstream
-  create = (req: express.Request, res: express.Response) => {
-    const user = req.user as User;
-
+  create = async (req: express.Request, res: express.Response) => {
+    const userId = (req.user as User).id;
     const file: any = req.file;
 
     cloudinary.uploader
@@ -20,62 +18,25 @@ class UploadController {
             size: file.bytes,
             ext: file.format,
             url: file.url,
-            user: {
-              id: user.id,
-              fullname: user.fullname,
-            },
+            user: userId,
           };
-=======
-  create = async (req: express.Request, res: express.Response) => {
-    const userId = (req.user as User).id;
-    const file: any = req.file;
 
-    cloudinary.uploader
-        .upload_stream({ resource_type: "auto" }, (error: any, file: any) => {
-          if (error) {
-            throw new Error(error);
-          } else {
-            const fileData = {
-              filename: file.original_filename,
-              size: file.bytes,
-              ext: file.format,
-              url: file.url,
-              user: userId,
-            };
->>>>>>> Stashed changes
+          const uploadedFile = new UploadModel(fileData);
 
-            const uploadedFile = new UploadModel(fileData);
-
-            try {
-              res.json({
-                status:"success",
-                file: uploadedFile
-              })
-            }catch (e){
-              return res.status(500).json({
-                status: "error",
-                message: error,
-              });
-            }
-
-
-            // uploadedFile
-            //   .save()
-            //   .then((fileObj: any) => {
-            //     res.json({
-            //       status: "success",
-            //       file: fileObj,
-            //     });
-            //   })
-            //   .catch((err) => {
-            //     res.json({
-            //       status: "error",
-            //       message: err,
-            //     });
-            //   });
+          try {
+            res.json({
+              status: "success",
+              file: uploadedFile,
+            });
+          } catch (e) {
+            return res.status(500).json({
+              status: "error",
+              message: error,
+            });
           }
-        })
-        .end(file.buffer);
+        }
+      })
+      .end(file.buffer);
   };
 }
 
